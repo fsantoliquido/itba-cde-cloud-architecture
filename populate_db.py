13a90713dd623f4cf841dcc264f649685ad15e09
Franco Santoliquido <fsantoliquido@MacBook-Air-de-Franco.local>
2024-07-02 12:21:15 -0400

    agregamos files por batches

--- populate_db.py
+++ populate_db.py
@@ -2,10 +2,14 @@ import psycopg2
 import os
 
 def main():
-    print("Conectando a la base de datos PostgreSQL...")
-    # Conectar a la base de datos PostgreSQL
+    print("Conectando a la base...")
+    """
+    La función se conecta a la db airlines. Toma del archivo "demo-small-en..." los dato y los inserta en las tablas. Luego se commitean los cmabios.
+    Por ultimo se cierra la conexión
+    """
+
     conn = psycopg2.connect(
-        host="db",  # Nombre del servicio del contenedor PostgreSQL en docker-compose.yml
+        host="db",
         database="airlines",
         user="fsantoliquido",
         password="itba1234"
@@ -13,15 +17,15 @@ def main():
     print("Conexión exitosa.")
     cur = conn.cursor()
     
-    print("Leyendo el archivo SQL...")
-    # Leer el archivo SQL
+    print("Leo el archivo SQL...")
+    
     with open('/scripts/transformed_inserts.sql', 'r') as file:
         sql = file.read()
     
-    print("Ejecutando sentencias SQL en lotes...")
-    # Ejecutar el archivo SQL en lotes
+    print("Ejecuto sentencias batch por faltamemoria...")
+    
     statements = sql.split(';')
-    batch_size = 100  # Tamaño del lote
+    batch_size = 100  # Tamaño del batch
     batch = []
 
     for statement in statements:
@@ -31,25 +35,25 @@ def main():
                 try:
                     cur.execute(';'.join(batch))
                     conn.commit()
-                    print(f"Lote de {batch_size} sentencias ejecutado.")
+                    print(f"Batch de {batch_size} ejecutado")
                 except psycopg2.Error as e:
-                    print(f"Error al ejecutar lote: {e}")
+                    print(f"Error al ejecutar batch: {e}")
                 batch = []
     
-    # Ejecutar cualquier sentencia restante
+
     if batch:
         try:
             cur.execute(';'.join(batch))
             conn.commit()
-            print("Sentencias restantes ejecutadas.")
+            print("Sentencias que no soninsert ejecutadas")
         except psycopg2.Error as e:
-            print(f"Error al ejecutar lote final: {e}")
+            print(f"Error al ejecutar: {e}")
 
-    print("Cerrando la conexión...")
-    # Cerrar la conexión
+    print("Cierro la la conexión..")
+
     cur.close()
     conn.close()
-    print("Finalizado con éxito.")
+    print("Finalizado con éxito")
 
 if __name__ == "__main__":
     main()
